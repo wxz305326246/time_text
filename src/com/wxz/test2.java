89fb30724e6188018bbacae37a7f4648737145ac
wxz <305326246@qq.com>
2020-04-17 11:24:03 +0800

    test2

---
+++ src/com/wxz/test2.java
@@ -0,0 +1,16 @@
+package com.wxz;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+import com.entity.Student;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Student student = new Student();
+			ArrayList arrayList = new ArrayList();
+	}
+
+}
